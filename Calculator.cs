f8fe8b1d821471cddda1d9b1fff12f7886989692
Artem Gorbatuk <gorbatuk.am@gmail.com>
2021-08-24 01:28:02 +0300

    init

---
+++ Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        private readonly Dictionary<string, Func<double, double, double>> operations = new()
+            {
+                { "+", (x, y) => x + y },
+                { "-", (x, y) => x - y },
+                { "*", (x, y) => x * y },
+                { "/", (x, y) => x / y },
+            };
+        public void DefineOperation(string op, Func<double, double, double> body)
+        {
+            if (operations.ContainsKey(op))
+                throw new ArgumentException($"Operation {op} is invalid", nameof(op));
+            operations.Add(op, body);
+        }
+        public double PerformOperation(string op, double x, double y)
+        {
+            if (!operations.ContainsKey(op))
+                throw new ArgumentException($"Operation {op} is invalid", nameof(op));
+            return operations[op](x, y);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            // интересным моментом здесь является не сам калькулятор а
+            // возможность создавать словарь фунций,
+            // возможность вызывать эти функции по ключу
+            // избегая изменения метода, избегая очередных if
+
+            var calc = new Calculator();
+            calc.DefineOperation("%", (x, y) => x % y);
+
+            var addition = calc.PerformOperation("+", 3.0, 2.0);
+            var subtraction = calc.PerformOperation("-", 3.0, 2.0);
+            var multiplication = calc.PerformOperation("*", 3.0, 2.0);
+            var division = calc.PerformOperation("/", 3.0, 2.0);
+            var remainder = calc.PerformOperation("%", 3.0, 2.0);
+
+            Console.WriteLine($"3 + 2 = {addition}");
+            Console.WriteLine($"3 - 2 = {subtraction}");
+            Console.WriteLine($"3 * 2 = {multiplication}");
+            Console.WriteLine($"3 / 2 = {division}");
+            Console.WriteLine($"3 % 2 = {remainder}");
+
+            Console.ReadLine();
+        }
+    }
+}
